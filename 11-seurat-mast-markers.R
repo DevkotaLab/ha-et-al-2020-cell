2c7da2f2235eff7e4ff7596f8cc06a5d07ce34a5
Michael Steinbaugh <mike@steinbaugh.com>
2020-04-30 20:10:15 -0400

    Rebase

--- 11-seurat-mast-markers.R
+++ 11-seurat-mast-markers.R
@@ -1,25 +1,10 @@
 source("_setup.R")
 
 # Using MAST method to find the markers, rather than Wilcoxon.
-<<<<<<< HEAD
-library(MAST)  # 1.14.0
-
-res <- "0.4"
-res_ident <- paste0("RNA_snn_res.", res)
-
-loadData(seurat_clustering_files, dir = file.path("rds", "2020-02-20"))
-results_dir <- initDir(file.path(
-    "results",
-    Sys.Date(),
-    "seurat-mast-markers",
-    res_ident
-))
-=======
 library(MAST)
 
 loadData(seurat_clustering_files, dir = file.path("rds", "2020-02-20"))
 rds_dir <- initDir(file.path(rds_dir, "seurat-mast-markers"))
->>>>>>> 4d8e303f6a42663e74095ddc765c02aa6bb94271
 
 ## Enable parallelization, but not inside RStudio.
 if (isTRUE(future::supportsMulticore())) {
@@ -38,13 +23,7 @@ seurat_mast_marker_files <-
             object <- readRDS(file)
             validObject(object)
             ## Previously, we used 0.4 in the 2018 analysis.
-<<<<<<< HEAD
-            Idents(object) <- res_ident
-=======
             Idents(object) <- resolution
-            ## Now seeing this warning regarding partial match:
-            ## Warning: partial match of 'coef' to 'coefold'
->>>>>>> 4d8e303f6a42663e74095ddc765c02aa6bb94271
             markers <- FindAllMarkers(object, test.use = "MAST")
             outfile <- file.path(rds_dir, basename(file))
             saveRDS(markers, file = outfile)
--- 12-seurat-wilcoxon-markers.R
+++ 12-seurat-wilcoxon-markers.R
@@ -1,21 +1,7 @@
 source("_setup.R")
-<<<<<<< HEAD
-
-res <- "0.4"
-res_ident <- paste0("RNA_snn_res.", res)
-
-loadData(seurat_clustering_files, dir = file.path("rds", "2020-02-20"))
-results_dir <- initDir(file.path(
-    "results",
-    Sys.Date(),
-    "seurat-wilcoxon-markers",
-    res_ident
-))
-=======
 
 loadData(seurat_clustering_files, dir = file.path("rds", "2020-02-20"))
 rds_dir <- initDir(file.path(rds_dir, "seurat-wilcoxon-markers"))
->>>>>>> 4d8e303f6a42663e74095ddc765c02aa6bb94271
 
 ## Enable parallelization, but not inside RStudio.
 if (isTRUE(future::supportsMulticore())) {
@@ -33,11 +19,7 @@ seurat_wilcoxon_marker_files <-
         FUN = function(file) {
             object <- readRDS(file)
             ## Previously, we used 0.4 in the 2018 analysis.
-<<<<<<< HEAD
-            Idents(object) <- res_ident
-=======
             Idents(object) <- resolution
->>>>>>> 4d8e303f6a42663e74095ddc765c02aa6bb94271
             markers <- FindAllMarkers(object, test.use = "wilcox")
             outfile <- file.path(rds_dir, basename(file))
             saveRDS(markers, file = outfile)
