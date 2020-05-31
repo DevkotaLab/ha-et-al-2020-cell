146160febde50f3c6d73497e08d338f0d899d859
Michael Steinbaugh <mike@steinbaugh.com>
2020-05-31 10:47:43 -0400

    Update plots

--- 27-proportional-cluster-count-rework.R
+++ 27-proportional-cluster-count-rework.R
@@ -50,7 +50,7 @@ plot <- ggplot(
         x = NULL,
         y = "relative cell count"
     )
-name <- "cellranger_ident_p_pct_stacked"
+name <- "cellranger_ident_plot_pct_stacked"
 ggsave(
     filename = file.path(results_dir, paste0(name, ".pdf")),
     plot = plot,
@@ -81,7 +81,7 @@ plot <- ggplot(
         x = "sample",
         y = "relative cell count"
     )
-name <- "cellranger_sample_name_p_pct_stacked"
+name <- "cellranger_sample_name_plot_pct_stacked"
 ggsave(
     filename = file.path(results_dir, paste0(name, ".pdf")),
     plot = plot,
@@ -112,7 +112,7 @@ plot <- ggplot(
         x = "condition",
         y = "relative cell count"
     )
-name <- "cellranger_condition_p_pct_stacked"
+name <- "cellranger_condition_plot_pct_stacked"
 ggsave(
     filename = file.path(results_dir, paste0(name, ".pdf")),
     plot = plot,
@@ -130,3 +130,98 @@ object <- readRDS(file.path(
     "seurat-clustering",
     "surecell_condition_cd_seurat.rds"
 ))
+Idents(object) <- resolution
+
+## All samples ----
+data <- metrics(object) %>%
+    group_by(ident) %>%
+    summarise (n = n())
+## Percent stacked bar plot.
+plot <- ggplot(
+    data = data,
+    mapping = aes(
+        x = "surecell",
+        y = n,
+        fill = ident
+    )
+) +
+    geom_bar(
+        color = "black",
+        position = "fill",  # Use "stack" for absolute
+        stat = "identity"
+    ) +
+    labs(
+        title = "all samples",
+        x = NULL,
+        y = "relative cell count"
+    )
+name <- "surecell_ident_plot_pct_stacked"
+ggsave(
+    filename = file.path(results_dir, paste0(name, ".pdf")),
+    plot = plot,
+    width = 2,
+    height = 7
+)
+assignAndSaveData(name = name, object = plot)
+
+## Per sample ----
+data <- metrics(object) %>%
+    group_by(sampleName, ident) %>%
+    summarise (n = n())
+plot <- ggplot(
+    data = data,
+    mapping = aes(
+        x = sampleName,
+        y = n,
+        fill = ident
+    )
+) +
+    geom_bar(
+        color = "black",
+        position = "fill",  # Use "stack" for absolute
+        stat = "identity"
+    ) +
+    labs(
+        title = "per sample",
+        x = "sample",
+        y = "relative cell count"
+    )
+name <- "surecell_sample_name_plot_pct_stacked"
+ggsave(
+    filename = file.path(results_dir, paste0(name, ".pdf")),
+    plot = plot,
+    width = 5,
+    height = 7
+)
+assignAndSaveData(name = name, object = plot)
+
+## Per condition ----
+data <- metrics(object) %>%
+    group_by(condition, ident) %>%
+    summarise (n = n())
+plot <- ggplot(
+    data = data,
+    mapping = aes(
+        x = condition,
+        y = n,
+        fill = ident
+    )
+) +
+    geom_bar(
+        color = "black",
+        position = "fill",  # Use "stack" for absolute
+        stat = "identity"
+    ) +
+    labs(
+        title = "per condition",
+        x = "condition",
+        y = "relative cell count"
+    )
+name <- "surecell_condition_plot_pct_stacked"
+ggsave(
+    filename = file.path(results_dir, paste0(name, ".pdf")),
+    plot = plot,
+    width = 5,
+    height = 7
+)
+assignAndSaveData(name = name, object = plot)
