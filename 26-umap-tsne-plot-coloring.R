3d690d015783576f34b90ea382288eff257792a1
Michael Steinbaugh <mike@steinbaugh.com>
2020-06-26 16:56:40 -0400

    Update plots

--- 26-umap-tsne-plot-coloring.R
+++ 26-umap-tsne-plot-coloring.R
@@ -1,3 +1,6 @@
+## UMAP and tSNE plots.
+## Updated 2020-06-26.
+
 source("_setup.R")
 
 cellranger <- readRDS(file.path(
@@ -5,7 +8,7 @@ cellranger <- readRDS(file.path(
     "2020-05-18",
     "cellranger_all_samples_seurat.rds"
 ))
-surecell <- readRDS(file.path(
+surecell_cd <- readRDS(file.path(
     "rds",
     "2020-02-20",
     "seurat-clustering",
@@ -15,7 +18,7 @@ surecell <- readRDS(file.path(
 ## Set the resolution.
 resolution <- import("resolution.txt", format = "lines")
 Idents(cellranger) <- resolution
-Idents(surecell) <- resolution
+Idents(surecell_cd) <- resolution
 
 ## Set the condition inside combined Cell Ranger dataset.
 ##
@@ -24,7 +27,7 @@ Idents(surecell) <- resolution
 ## - D1: Non-IBD control
 ## - H1: Non-IBD control
 ##
-## Match the factor levels, so the plot colors are consistent with surecell.
+## Match the factor levels, so the plot colors are consistent with surecell_cd.
 cellranger@meta.data$condition <-
     ifelse(
         test = cellranger@meta.data$sampleID %in% c("A04", "A16"),
@@ -44,7 +47,7 @@ colnames(cellranger@meta.data)
 ## [19] "RNA_snn_res.0.8"       "RNA_snn_res.1"         "RNA_snn_res.1.2"
 ## [22] "seurat_clusters"       "condition"
 
-colnames(surecell@meta.data)
+colnames(surecell_cd@meta.data)
 ##  [1] "orig.ident"            "nCount_RNA"            "nFeature_RNA"
 ##  [4] "condition"             "description"           "log10FeaturesPerCount"
 ##  [7] "mitoRatio"             "nCoding"               "nCount"
@@ -55,7 +58,7 @@ colnames(surecell@meta.data)
 ## [22] "RNA_snn_res.0.4"       "RNA_snn_res.0.6"       "RNA_snn_res.0.8"
 ## [25] "RNA_snn_res.1"         "RNA_snn_res.1.2"       "seurat_clusters"
 
-## > saveData(cellranger, surecell)
+## > saveData(cellranger, surecell_cd)
 
 ## Note that these plots are too large to save to disk.
 ## Just regenerate, if necessary.
@@ -159,12 +162,12 @@ ggsave(
 ggsave(
     filename = file.path(
         results_dir,
-        "surecell_tsne_ident.pdf"
+        "surecell_cd_tsne_ident.pdf"
     ),
     width = width,
     height = height,
     plot = plotTSNE(
-        object = surecell,
+        object = surecell_cd,
         interestingGroups = NULL,
         pointSize = point_size,
         labelSize = label_size
@@ -173,12 +176,12 @@ ggsave(
 ggsave(
     filename = file.path(
         results_dir,
-        "surecell_tsne_sample_name.pdf"
+        "surecell_cd_tsne_sample_name.pdf"
     ),
     width = width,
     height = height,
     plot = plotTSNE(
-        object = surecell,
+        object = surecell_cd,
         interestingGroups = "sampleName",
         pointSize = point_size,
         labelSize = label_size
@@ -187,12 +190,12 @@ ggsave(
 ggsave(
     filename = file.path(
         results_dir,
-        "surecell_tsne_tissue_status.pdf"
+        "surecell_cd_tsne_tissue_status.pdf"
     ),
     width = width,
     height = height,
     plot = plotTSNE(
-        object = surecell,
+        object = surecell_cd,
         interestingGroups = "tissueStatus",
         pointSize = point_size,
         labelSize = label_size
@@ -203,12 +206,12 @@ ggsave(
 ggsave(
     filename = file.path(
         results_dir,
-        "surecell_umap_ident.pdf"
+        "surecell_cd_umap_ident.pdf"
     ),
     width = width,
     height = height,
     plot = plotUMAP(
-        object = surecell,
+        object = surecell_cd,
         interestingGroups = NULL,
         pointSize = point_size,
         labelSize = label_size
@@ -217,12 +220,12 @@ ggsave(
 ggsave(
     filename = file.path(
         results_dir,
-        "surecell_umap_sample_name.pdf"
+        "surecell_cd_umap_sample_name.pdf"
     ),
     width = width,
     height = height,
     plot = plotUMAP(
-        object = surecell,
+        object = surecell_cd,
         interestingGroups = "sampleName",
         pointSize = point_size,
         labelSize = label_size
@@ -231,12 +234,12 @@ ggsave(
 ggsave(
     filename = file.path(
         results_dir,
-        "surecell_umap_tissue_status.pdf"
+        "surecell_cd_umap_tissue_status.pdf"
     ),
     width = width,
     height = height,
     plot = plotUMAP(
-        object = surecell,
+        object = surecell_cd,
         interestingGroups = "tissueStatus",
         pointSize = point_size,
         labelSize = label_size
--- 28-paper-figure-draft.R
+++ 28-paper-figure-draft.R
@@ -1,5 +1,3 @@
-source("_setup.R")
-
 ## Improve consistency of colors in figures, per Suzanne's request.
 ## Updated 2020-06-26.
 ##
@@ -7,5 +5,11 @@ source("_setup.R")
 ## Crohn's orange
 ##
 ## Need to set the factor levels in UMAP/tSNE plots for consistency.
+##
+## Relevant dates:
+## - 2020-05-28: UMAP and t-SNE plots
+## - 2020-05-31: proportional stacked bar plots
 
-## 2020-05-31
+
+source("_setup.R")
+
